b7f31dd80d5c005e6a99d1d4533a6caea3e82385
rajnish2023 <123153366+rajnish2023@users.noreply.github.com>
2025-07-30 17:58:17 +0530

    Update next.config.ts with Disable ESLint

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   eslint: {
-    ignoreDuringBuilds: true, // ✅ Disable ESLint on build
-  },
-  // Enable the /app directory (app router)
-  experimental: {
-    appDir: true,
+    ignoreDuringBuilds: true, // ✅ Disable ESLint check during build
   },
 };
 
 export default nextConfig;
-
